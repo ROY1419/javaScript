99e54cccdc0c4efeb5feb5e3628985e55f0c6fa6
Roy1419 <royshubham471@gmail.com>
2024-12-21 23:06:49 +0530

    this commit is for reduce method in loop

---
+++ loops/reduceLoop.js
@@ -0,0 +1,35 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+// reduce() method take two argument one is accumlator and cuurent value = reduce((acc, curv) => (acc + curv), 0)
+// by defalut it take a vaslue which is considering value here the value is 0 by cahnging the incrementation
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
